7fa27446f75d47bc3bce3becd3158358eef1fca3
AharonSambol <75334174+AharonSambol@users.noreply.github.com>
2026-06-13 23:57:25 +0300

    squirrel-startup?

--- main.js
+++ main.js
@@ -1,5 +1,85 @@
+if (require('electron-squirrel-startup')) return;
+
+// const squirrel_app = require('app');
+
+// if (handleSquirrelEvent()) {
+//     // squirrel event handled and app will exit in 1000ms, so don't do anything else
+//     return;
+// }
+
+// function handleSquirrelEvent() {
+//     if (process.argv.length === 1) {
+//         return false;
+//     }
+
+//     const ChildProcess = require('child_process');
+//     const path = require('path');
+
+//     const appFolder = path.resolve(process.execPath, '..');
+//     const rootAtomFolder = path.resolve(appFolder, '..');
+//     const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+//     const exeName = path.basename(process.execPath);
+
+//     const spawn = function (command, args) {
+//         let spawnedProcess, error;
+
+//         try {
+//             spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
+//         } catch (error) { }
+
+//         return spawnedProcess;
+//     };
+
+//     const spawnUpdate = function (args) {
+//         return spawn(updateDotExe, args);
+//     };
+
+//     const squirrelEvent = process.argv[1];
+//     switch (squirrelEvent) {
+//         case '--squirrel-install':
+//         case '--squirrel-updated':
+//             // Optionally do things such as:
+//             // - Add your .exe to the PATH
+//             // - Write to the registry for things like file associations and
+//             //   explorer context menus
+
+//             // Install desktop and start menu shortcuts
+//             spawnUpdate(['--createShortcut', exeName]);
+
+//             setTimeout(squirrel_app.quit, 1000);
+//             return true;
+
+//         case '--squirrel-uninstall':
+//             // Undo anything you did in the --squirrel-install and
+//             // --squirrel-updated handlers
+
+//             // Remove desktop and start menu shortcuts
+//             spawnUpdate(['--removeShortcut', exeName]);
+
+//             setTimeout(squirrel_app.quit, 1000);
+//             return true;
+
+//         case '--squirrel-obsolete':
+//             // This is called on the outgoing version of your app before
+//             // we update to the new version - it's the opposite of
+//             // --squirrel-updated
+
+//             squirrel_app.quit();
+//             return true;
+//     }
+// };
+
+
+
 const { app, BrowserWindow, ipcMain, dialog, Menu, clipboard } = require('electron');
 const path = require('node:path');
+
+// app.whenReady().then(() => {
+//   // Set your App User Model ID
+//     app.setAppUserModelId("com.yourcompany.yourapp");
+    
+//     createWindow();
+// });
 
 ipcMain.handle('select-project-folder', async (event, options) => {
     return dialog.showOpenDialog(options);
@@ -11,8 +91,8 @@ ipcMain.handle('show-context-menu', async (event, info) => {
         template.push({
             label: 'Copy',
             click: () => {
-                try { 
-                    
+                try {
+
                     console.log('Copying to clipboard:', info.selection, info.selection.length);
                     clipboard.writeText(info.selection);
                 } catch (err) {
@@ -66,7 +146,7 @@ ipcMain.handle('show-context-menu', async (event, info) => {
             }
         }
     });
-    
+
     const menu = Menu.buildFromTemplate(template);
     const win = BrowserWindow.fromWebContents(event.sender);
     menu.popup({ window: win });
